dabc30fb295b3672424410432f17677538253cff
ThaminduR <thamindu.randil@gmail.com>
2020-07-01 19:26:16 +0530

    Server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const mysql = require('mysql');
+const app = express();
+var cookieParser = require('cookie-parser')
+
+require('dotenv').config()
+
+const port = process.env.PORT
+const address = process.env.address
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const index = require('./routes/indexRoutes')
+app.use('/', index);
+
+
+
+app.listen(port, address, () => console.log("App started. Running on localhost:" + process.env.PORT))
